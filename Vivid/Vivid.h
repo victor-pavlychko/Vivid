b69d1afa9b9d3fcf4249cb0eddeecd29ad8c5a95
Victor Pavlychko <victor.pavlychko@gmail.com>
2018-05-28 23:14:02 +0300

    Added project for Carthage

---
+++ Vivid/Vivid.h
@@ -0,0 +1,38 @@
+//
+//  Vivid.h
+//  Vivid
+//
+//  Created by Victor Pavlychko on 5/28/18.
+//  Copyright © 2018 address.wtf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vivid.
+FOUNDATION_EXPORT double VividVersionNumber;
+
+//! Project version string for Vivid.
+FOUNDATION_EXPORT const unsigned char VividVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vivid/PublicHeader.h>
+
+#include <Vivid/YUCIBilateralFilter.h>
+#include <Vivid/YUCIBlobsGenerator.h
+#include <Vivid/YUCICLAHE.h
+#include <Vivid/YUCIColorLookup.h
+#include <Vivid/YUCICrossZoomTransition.h
+#include <Vivid/YUCIFilmBurnTransition.h
+#include <Vivid/YUCIFilterConstructor.h
+#include <Vivid/YUCIFilterPreviewGenerator.h
+#include <Vivid/YUCIFilterUtilities.h
+#include <Vivid/YUCIFlashTransition.h
+#include <Vivid/YUCIFXAA.h
+#include <Vivid/YUCIHistogramEqualization.h
+#include <Vivid/YUCIReflectedTile.h
+#include <Vivid/YUCIReflectedTileROICalculator.h
+#include <Vivid/YUCIRGBToneCurve.h
+#include <Vivid/YUCISkyGenerator.h
+#include <Vivid/YUCIStarfieldGenerator.h
+#include <Vivid/YUCISurfaceBlur.h
+#include <Vivid/YUCITriangularPixellate.h
+#include <Vivid/YUCIUtilities.h
